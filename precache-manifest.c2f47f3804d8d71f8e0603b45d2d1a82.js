174641ee69808755a58a7a90198ab2ae30411022
Mher Hovhannisyan <mh3r99@gmail.com>
2022-03-10 17:59:03 +0400

    Updates

---
+++ precache-manifest.c2f47f3804d8d71f8e0603b45d2d1a82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6520a13a97d6ecca812e7b07df1473e",
+    "url": "/github-search-react-app/index.html"
+  },
+  {
+    "revision": "4c2719ab1bbac84cadb8",
+    "url": "/github-search-react-app/static/css/main.ceb9dfde.chunk.css"
+  },
+  {
+    "revision": "0de2912ab0825960b646",
+    "url": "/github-search-react-app/static/js/2.5c0ccb22.chunk.js"
+  },
+  {
+    "revision": "4c2719ab1bbac84cadb8",
+    "url": "/github-search-react-app/static/js/main.c8d414df.chunk.js"
+  },
+  {
+    "revision": "20b89aaeb899417748db",
+    "url": "/github-search-react-app/static/js/runtime~main.c653bf15.js"
+  }
+]);
